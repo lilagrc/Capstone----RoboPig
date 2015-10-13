46745d946f5951a88afdf5479b63abca86600907
Lila Garcia <lilagrc@gmail.com>
2015-10-12 21:08:26 -0700

    Added controller tests for session create and user index

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "#create" do
+    context "when using omniauth developer authorization" do
+      context "when successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:developer] }
 
+        it "redirects to home page" do
+          get :create, provider: :developer
+
+          expect(response).to redirect_to root_path
+        end
+
+        it "creates a user" do
+          expect { get :create, provider: :developer}.to change(User, :count).by(1)
+        end
+
+        it "assigns the session[:user_id]" do
+          get :create, provider: :developer
+
+          expect(session[:user_id]).to eq assigns(:user).id
+        end
+      end
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "#index" do
+    let(:log_in) {
+    @logged_user = create :user
+    session[:user_id] = @logged_user.id }
 
+    before :each do
+      log_in
+      get :index
+    end
+
+    it "renders the index page" do
+      expect(subject).to render_template :index
+      expect(response).to have_http_status(200)
+    end
+
+    it "sets @user to the current user" do
+      expect(session[:user_id]).to eq @logged_user.id
+    end
+  end
 end
