63321a912a8733250c2ca123a2a6477712ecbdef
Lila Garcia <lilagrc@gmail.com>
2015-10-14 15:16:30 -0700

    added uid to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007203450) do
+ActiveRecord::Schema.define(version: 20151014221550) do
 
   create_table "feedings", force: :cascade do |t|
     t.datetime "date"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151007203450) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "uid"
   end
 
 end
