1b6b956ae7dbcd6098c55dbc302aa90c48bf3ac3
Lila Garcia <lilagrc@gmail.com>
2015-10-16 14:50:06 -0700

    WIP

--- app/controllers/pi_controller.rb
+++ app/controllers/pi_controller.rb
@@ -2,11 +2,13 @@ class PiController < ApplicationController
 
 # create method to update data sent to turn on LEDs
 def new_request
-  if params["request"]["body"] == "true"
-    session["request"] = true
-  else
-    session["request"] = false
-  end
+#   if params["request"]["body"] == "true"
+#     session["request"] = true
+#   else
+#     session["request"] = false
+#   end
+
+  session["request"] = params["request"]["body"]
   redirect_to root_path
 end
 
