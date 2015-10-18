912a4956b3e86e062cbe4f2711c7402002ff962d
Lila Garcia <lilagrc@gmail.com>
2015-10-18 16:24:10 -0700

    commented out twitter things... probs not necessary

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,10 @@
 class Tweet < ActiveRecord::Base
-  belongs_to :user
-  before_create :post_to_twitter
+  # belongs_to :user
+  # before_create :post_to_twitter
 
-  validates :user_id, :body, presence: true
+  # validates :user_id, :body, presence: true
 
-  def post_to_twitter
-    user.twitter.update(body)
-  end
+  # def post_to_twitter
+  #   user.twitter.update(body)
+  # end
 end
