fc2cf11c3963fcb8496892c786d713e1fa84cfc6
Lila Garcia <lilagrc@gmail.com>
2015-10-19 22:01:18 -0700

    Adds ability for tokens for api

---
+++ api.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+def send_confirmation():
+  url = 'http://www.robofeedpet.com/pi_confirmation'
+  payload = {'request': 'success'}
+  r = requests.put(url, json=payload)
+  return
+
+# r = requests.put('http://www.robofeedpet.com/run_pi', data = {"feed":"yes"})
+r = requests.get('http://www.robofeedpet.com/run_pi', headers={'Authorization': 'Token token='})
+
+r = r.json()
+print r
+
+if r["request"] == "feed":
+  import servo1;
+  send_confirmation();
+else:
+  print "No food now"
+
+
+
+
+
+
+
---
+++ app/controllers/api/v1/requests_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::RequestsController < ApplicationController
+  before_action :authenticate
+
+  # method to send pi information from request db
+  def run_pi
+    request = Request.first.body
+
+    res = { request: request}
+    render json: res
+  end
+
+  def confirmation
+    @request = Request.find(1)
+
+    if params["request"] == "success"
+      flash.now[:notice] = "Your pet has been fed!"
+      @request.body = "none"
+      @request.save
+    end
+    # request = Request.find(1)
+
+    # Request.update()
+    #remove body data from db if response is success
+    #success message comes in as post request from Pi
+    render :nothing => true
+  end
+end
