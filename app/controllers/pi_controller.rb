1bea0ba9d8480258fdd758f4a34d598f4cd85569
Lila Garcia <lilagrc@gmail.com>
2015-10-25 11:30:24 -0700

    testing new confirmation/feeding

--- app/controllers/pi_controller.rb
+++ app/controllers/pi_controller.rb
@@ -7,8 +7,6 @@ class PiController < ApplicationController
     request.body = "feed"
     request.save
 
-    add_feeding
-
     redirect_to root_path
   end
 
@@ -45,20 +43,6 @@ class PiController < ApplicationController
     request.save
     flash[:notice] = "Your feedings have been cancelled"
     redirect_to root_path
-  end
-
-  private
-
-  def add_feeding
-    id = session[:user_id]
-    user = User.find_by(id: id)
-    #only handles one pet per user for now
-    pet_id = user.pets.first.id
-    new_feeding = Feeding.new
-    new_feeding.pet_id = pet_id
-    # new_feeding.date = Time.now.strftime(%I:%M,%m/%d/%y)
-    new_feeding.amount = 1
-    new_feeding.save
   end
 
 end
