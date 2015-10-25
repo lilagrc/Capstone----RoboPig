a968126cacaf49254eed17c8a35e400c185c8c6d
Lila Garcia <lilagrc@gmail.com>
2015-10-25 12:50:13 -0700

    WIP

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,12 +7,12 @@ class PetsController < ApplicationController
 
   def create
     user = User.find(session[:user_id])
-    @pet = Pet.create(create_params[:pet])
-    @pet.api_key = Pet.generate_api_key
-    user.pet_id = @pet.id
+    pet = Pet.create(create_params[:pet])
+    pet.api_key = Pet.generate_api_key
+    user.pet_id = pet.id
     user.save
 
-    if @pet.save
+    if pet.save
         flash[:notice] = "Congratulations! You have now registered with Robo Pets. Please see your profile page for an API token which can be used for your robo device."
         redirect_to user_path(session[:user_id])
     else
