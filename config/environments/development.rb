180fdc9d57f815307021049d758a84f095280d08
Lila Garcia <lilagrc@gmail.com>
2015-10-29 12:06:03 -0700

    trying to fix timezone thing so feedings have correct time

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-  config.time_zone = 'Pacific Time (US & Canada)'
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
